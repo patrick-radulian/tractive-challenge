834dd0be233de250f3282767e1b48dc442eb7ebb
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-14 21:05:12 +0200

    Add buttons to set values

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App.module.css';
 import PetBMI from './components/pet-bmi/pet-bmi';
 
 const BMIRanges = [
-    { from: 0, to: 29.99, color: "#0D78C8", label: "Underweight" },
+    { from: 0, to: 29.99, color: "#C8680D", label: "Underweight" },
     { from: 30, to: 90, color: "#058F22", label: "Normal" },
     { from: 90.01, to: 120, color: "#8A0303", label: "Overweight" },
 ]
@@ -17,6 +17,12 @@ const App = () => {
                 <span>Pet BMI Value:</span>
                 <input className={styles.input} type="number" value={value} onChange={(e) => setValue(Number(e.target.value))} />
             </label>
+
+            <div className={styles.actions}>
+                <button onClick={() => setValue(20)}>Set to 20</button>
+                <button onClick={() => setValue(60)}>Set to 60</button>
+                <button onClick={() => setValue(100)}>Set to 100</button>
+            </div>
 
             <div className={styles.container}>
                 <h3>Mr. Nash's BMI is</h3>
